ac52bb8e0ec7dd7bd522d38bf57997a191b96f1a
Mikyle Reyes <reyesmikyle17@gmail.com>
2022-01-16 17:30:33 +0800

    Make feedback stats and list return data using useContext hook

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -9,6 +9,16 @@ export const FeedbackProvider = ({ children }) => {
       text: 'This item is from context',
       rating: 10,
     },
+    {
+      id: 2,
+      rating: 9,
+      text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
+    },
+    {
+      id: 3,
+      rating: 8,
+      text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
+    },
   ])
 
   return (
