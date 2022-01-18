106275927767b53ac1034d2faa11032e79036d3b
Mikyle Reyes <reyesmikyle17@gmail.com>
2022-01-18 15:35:46 +0800

    Fetch data from json-server backend

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,33 +1,32 @@
 import { v4 as uuidv4 } from 'uuid'
-import { createContext, useState } from 'react'
+import { createContext, useState, useEffect } from 'react'
 
 const FeedbackContext = createContext()
 
 export const FeedbackProvider = ({ children }) => {
-  const [feedback, setFeedback] = useState([
-    {
-      id: 1,
-      text: 'This item is feedback item 1',
-      rating: 10,
-    },
-    {
-      id: 2,
-      rating: 9,
-      text: 'This item is feedback item 2',
-    },
-    {
-      id: 3,
-      rating: 8,
-      text: 'This item is feedback item 3',
-    },
-  ])
-
-  // Add feedback
+  const [isLoading, setIsLoading] = useState(true)
+  const [feedback, setFeedback] = useState([])
   const [feedbackEdit, setFeedbackEdit] = useState({
     item: {},
     edit: false,
   })
 
+  useEffect(() => {
+    fetchFeedback()
+  }, [])
+
+  // Fetch feedback
+  const fetchFeedback = async () => {
+    const response = await fetch(
+      `http://localhost:5000/feedback?_sort=id&_order=desc`
+    )
+    const data = await response.json()
+
+    setFeedback(data)
+    setIsLoading(false)
+  }
+
+  // Add feedback
   const addFeedback = (newFeedback) => {
     newFeedback.id = uuidv4()
     setFeedback([newFeedback, ...feedback]) // This adds the newFeedback.id to the state including the feedback that's already in the state using the spread operator
@@ -60,6 +59,7 @@ export const FeedbackProvider = ({ children }) => {
       value={{
         feedback: feedback,
         feedbackEdit, // value to be edited
+        isLoading,
         deleteFeedback,
         addFeedback,
         editFeedback, // function to edit
